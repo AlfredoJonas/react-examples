03dc95868009c64b08b5df67f15aaa98c80e321f
Jonas Gonzalez <alfredojonas94@gmail.com>
2018-03-14 17:29:09 -0400

    second commit

---
+++ src/App.2.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {items: []}
+  }
+
+  componentWillMount(){
+    fetch('https://swapi.co/api/people/?format=json')
+    .then( response => response.json() )
+    .then( ({results: items}) => this.setState({items}))
+  }
+
+  filter(e){
+    this.setState({filter: e.target.value})
+  }
+
+  render() {
+    let items = this.state.items;
+    if(this.state.filter){
+      items = items.filter(item => item.name.toLowerCase().includes(this.state.filter.toLowerCase()))
+    }
+    return (
+      <div>
+        <input type="text" onChange={this.filter.bind(this)}/>
+        {items.map( item =>
+          <h4 key={item.name}>{item.name}</h4>
+        )}
+      </div>
+    )
+  }
+
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App';
+// import App from './App.1';
+// import App from './App.2';
+// import App from './App.3';
+// import App from './App.4';
+// import App from './App.5';
+// import App from './App.6';
+// import App from './App.7';
+import App from './App.router';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
